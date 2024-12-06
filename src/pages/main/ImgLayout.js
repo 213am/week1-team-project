ee2d2a3b01a31235be3cb2d3ca2d859b0d4c4c47
k-risu <kkd4869@gmail.com>
2024-12-06 18:05:51 +0900

    [작업]

---
+++ src/pages/main/ImgLayout.js
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled";
+
+export const ImgLayout = styled.div`
+  display: flex;
+  height: 300px;
+  margin: 0 auto;
+  align-items: center;
+  justify-content: center;
+`;
---
+++ src/pages/main/MainLayout.js
@@ -0,0 +1,7 @@
+import styled from "@emotion/styled";
+
+export const MainLayout = styled.div`
+  /* display: block; */
+  height: 300px;
+  margin: 0 auto;
+`;
